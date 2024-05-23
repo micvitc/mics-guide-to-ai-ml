15933033d2cadebad8d775c365b12c410e4a1676
norsu <mohammedameen9011@gmail.com>
2024-05-23 14:35:57 +0530

    deployment fix 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://micvitc.github.io/mics-guide-to-ai-ml/',
+  base: '/mics-guide-to-ai-ml/',
   integrations: [starlight({
     title: 'Guide to AI/ML',
     logo: {
